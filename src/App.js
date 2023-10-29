b508694ec3db7d8bcf0b422138c0822ef6361554
Inferno <nishantkatekar58@gmail.com>
2023-10-29 10:10:11 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SimonGame from "./components/SimonGame";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import LeaderBoard from "./components/LeaderBoard";
 
 function App() {
   return (
